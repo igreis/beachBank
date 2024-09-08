71eb04901e59ab5f0c5716aeefc3f6349c72f2eb
igor <igalvesreis2121@gmail.com>
2024-09-08 12:49:47 -0300

    Updates

--- src/components/ui/react-label.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Root = React.forwardRef(({ className, ...props }, ref) => {
-  return <label ref={ref} className={className} {...props} />
-})
-
-Root.displayName = 'Label'
-
-export { Root }
