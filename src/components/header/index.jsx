1cf7fb1dbad9c73c19c5740dbe03bae82c106ced
igor <igalvesreis2121@gmail.com>
2024-12-03 16:13:08 -0300

    trocado a por link

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react"
 import { Button } from "../ui/button" 
-import { Link } from "react-router-dom"
 import useStore from "../../store/store"
+import { Link } from 'react-router-dom'
 import { Waves, Sun, Moon } from 'lucide-react'
 
 export const Header = () => {
@@ -18,12 +18,12 @@ export const Header = () => {
           </Link>
         </div>
         <div className="hidden md:flex space-x-4">
-          <a href="/beachBank/" className="text-gray-600 dark:text-gray-300 hover:text-blue-600 dark:hover:text-blue-400">Início</a>
-          <a href="#" className="text-gray-600 dark:text-gray-300 hover:text-blue-600 dark:hover:text-blue-400">Conta Digital</a>
-          <a href="/beachBank/arvore" className="text-gray-600 dark:text-gray-300 hover:text-blue-600 dark:hover:text-blue-400">Árvore do Dinheiro</a>
-          <a href="/beachBank/investimentos" className="text-gray-600 dark:text-gray-300 hover:text-blue-600 dark:hover:text-blue-400">Investimentos</a>
-          <a href="/beachBank/despesas" className="text-gray-600 dark:text-gray-300 hover:text-blue-600 dark:hover:text-blue-400">Despesas</a>
-          <a href="/beachBank/funcao" className="text-gray-600 dark:text-gray-300 hover:text-blue-600 dark:hover:text-blue-400">Funções</a>
+          <Link to={"/"} className="text-gray-600 dark:text-gray-300 hover:text-blue-600 dark:hover:text-blue-400">Início</Link>
+          <Link to="#" className="text-gray-600 dark:text-gray-300 hover:text-blue-600 dark:hover:text-blue-400">Conta Digital</Link>
+          <Link to="/arvore" className="text-gray-600 dark:text-gray-300 hover:text-blue-600 dark:hover:text-blue-400">Árvore do Dinheiro</Link>
+          <Link to="/investimentos" className="text-gray-600 dark:text-gray-300 hover:text-blue-600 dark:hover:text-blue-400">Investimentos</Link>
+          <Link to="/despesas" className="text-gray-600 dark:text-gray-300 hover:text-blue-600 dark:hover:text-blue-400">Despesas</Link>
+          <Link to="/funcao" className="text-gray-600 dark:text-gray-300 hover:text-blue-600 dark:hover:text-blue-400">Funções</Link>
         </div>
         <Button variant="outline" onClick={toggleDarkMode} className="text-gray-600 dark:text-gray-300 border-gray-300 dark:border-gray-600 hover:bg-gray-100 dark:hover:bg-gray-700">
           {darkMode ? <Sun className="mr-2 h-4 w-4" /> : <Moon className="mr-2 h-4 w-4" />}
